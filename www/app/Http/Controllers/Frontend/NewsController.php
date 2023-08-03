cd463a246971427e1cf2be022b7d0e2276019055
Kajal baldha <kajal.baldha@tiez.nl>
2023-08-03 18:16:15 +0530

    news

---
+++ www/app/Http/Controllers/Frontend/NewsController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\News;
+use App\Models\Page;
+use Illuminate\Http\Request;
+use Artesaos\SEOTools\Facades\SEOMeta;
+use Artesaos\SEOTools\Facades\SEOTools;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+class NewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+
+        $table = resolve('news-repo')->renderHtmlNewsTable($this->getParamsForFilter($request));
+
+        return view('frontend.news', compact('table'));
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+    public function getParamsForFilter(Request $request)
+    {
+        $previousUrl = parse_url(url()->previous());
+        $params = [];
+
+        if (request()->routeIs('frontend.news') || !isset($previousUrl['query'])) {
+            $params['query_str'] = $request->query_str ?? '';
+            $params['page'] =  $request->page ?? 0;
+            $params['path'] =  \Illuminate\Support\Facades\Request::fullUrl();
+        } else {
+            parse_str($previousUrl['query'], $params);
+            $params['path'] =  url()->previous();
+        }
+        return $params;
+    }
+
+}
--- www/resources/views/frontend/news.blade.php
+++ www/resources/views/frontend/news.blade.php
@@ -18,31 +18,8 @@
     <div class="new-container">
         <div class="news-list">
             <p class="content-title">News</p>
-            <div class="news-list-wrapper">
-                @foreach ($news as $news)
-                    <div class="news-box ">
-                        <div class="images-box">
-                            <a href="singles-new.html" class="images">
-                                <img src={{ asset('storage/news') . '/' . $news->image }} alt="News 1">
-                            </a>
-                        </div>
-                        <div class="content-news">
-                            <a href="{{ route('backend.news', $news->title) }}">
-                                {{ $news->title }}
-                            </a>
-                        </div>
-                    </div>
-                @endforeach
+                {!! $table !!}
 
-            </div>
-            <div class="pagination">
-                <a href="#" class="page-numbers prev">Previous</a>
-                <a href="#" class="page-numbers current">1</a>
-                <a href="#" class="page-numbers">2</a>
-                <a href="#" class="page-numbers">...</a>
-                <a href="#" class="page-numbers">20</a>
-                <a href="#" class="next page-numbers">Next</a>
-            </div>
         </div>
     </div>
     <div class="history-slider-main reveal">
